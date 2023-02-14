1a1edd1f2bd824a9a9b51d9c116ec1534adb0986
Starzen Garters <starzen@blockquote.rip>
2023-02-13 20:12:34 -0500

    Adding a method to let us get secrets from the ENV or user-secrets.

--- monitor/Program.cs
+++ monitor/Program.cs
@@ -1,8 +1,36 @@
-﻿namespace monitor;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace monitor;
 class Program
 {
+    private static string _BearerTokenKey = "TwitterV2Bearer";
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        var bearer = GetBearerToken();
+    }
+
+    public static string GetBearerToken()
+    {
+        var result = System.Environment.GetEnvironmentVariable(_BearerTokenKey, EnvironmentVariableTarget.User);
+
+        if(string.IsNullOrWhiteSpace(result))
+        {
+            // We didn't find it right in the ENV, build up a config and see if we can suck it out of a secret vault.
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+            
+            var configurationRoot = builder.Build();
+
+            result = configurationRoot[_BearerTokenKey];
+            
+            if(string.IsNullOrWhiteSpace(result))
+            {
+                throw new Exception($"Could not find a value for the key \"{_BearerTokenKey}\"");
+            }
+        }
+
+        return result;
     }
 }
